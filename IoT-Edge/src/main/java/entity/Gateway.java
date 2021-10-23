e1389452628149743f6873f968458f77cce64df2
cristinaStratan1 <56672529+cristinaStratan1@users.noreply.github.com>
2021-10-23 12:23:15 +0300

    Update Gateway.java

--- IoT-Edge/src/main/java/entity/Gateway.java
+++ IoT-Edge/src/main/java/entity/Gateway.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
@@ -10,6 +11,8 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 
 import impl.CCMImpl;
+import io.ipfs.api.IPFS;
+import io.ipfs.multihash.Multihash;
 
 public class Gateway implements MqttCallback {
 
@@ -17,22 +20,23 @@ public class Gateway implements MqttCallback {
 //	 a shared long-term symmetric master key, called Km; 
 	static final String Km = "SymmKeyLongTerm";
 	MqttClient thisClient;
-	//	and a second, shared short-term symmetric session key, called Ks
+	// and a second, shared short-term symmetric session key, called Ks
 //	The specific initial value of Ks, that is added manually is denoted by Kiks.
 	static String Kiks = "communicationInitValue";
 
 	static String nonce1 = secureRandom();
+	static ArrayList<Integer> IoTDevicesIDs;
 
 	static String IDg = "01";
 	static String nonce2 = secureRandom();
 	static String iv = "0";
 	static CCMImpl ccm;
 	static String encryptedNonce1;
-//	 Random set of sequence numbers of session (i - 1) frames to be used in session i’s session key update
+//	 Random set of sequence numbers of session (i - 1) frames to be used in session iâ€™s session key update
 //	static String RandFirmSeqsi = "randfirm";
 	static String[] RandFirmSeqsi;
 
-//	 Random set of sequence numbers of session i frames to be used in session (i + 1)’s session key update
+//	 Random set of sequence numbers of session i frames to be used in session (i + 1)â€™s session key update
 	static String[] RandFirmSeqsiPlus1;
 	static String RandomSequence = "";
 
@@ -64,6 +68,7 @@ public class Gateway implements MqttCallback {
 	public void listenIoTDevices() throws MqttException {
 		thisClient.setCallback(this);
 		thisClient.subscribe("IoTDevice");
+		IoTDevicesIDs = new ArrayList<Integer>();
 	}
 
 	public void listentoFirmwareUpdate() throws MqttException {
@@ -75,6 +80,7 @@ public class Gateway implements MqttCallback {
 
 	// publisher
 	public void sendAuthetication(MqttClient client, String idEdge) throws MqttPersistenceException, MqttException {
+//		String idEdge = "my edge id: 01";
 		MqttMessage message = new MqttMessage();
 		message.setPayload(idEdge.getBytes());
 		client.publish("EdgeDevice", message);
@@ -88,35 +94,44 @@ public class Gateway implements MqttCallback {
 
 	public void messageArrived(String topic, MqttMessage message) throws Exception {
 		System.out.println("Edge device received:" + message);
-		String[] digestMessage= message.toString().split(" ");
+		String[] digestMessage = message.toString().split(" ");
 		if (digestMessage[0].equals("new_update")) {
 //			startAuth();
-		}
-		else if(digestMessage[0].equals("auth_request"))
-		{
-			String decodedMAC= ccm.encode(Kiks, digestMessage[0]+" "+digestMessage[1]+ " "+digestMessage[2]+ " "+digestMessage[3]);
-			if(digestMessage[digestMessage.length-1].equals(decodedMAC))
-			{
-				startAuth( ccm.ccmaes(false, digestMessage[3], Km, iv));
+			IPFS ipfs = new IPFS("localhost", 5001);
+			try {
+				Multihash filePointer = Multihash.fromBase58(digestMessage[2]);
+				byte[] fileContents = ipfs.cat(filePointer);
+				byte[] two = "UPDATE ".getBytes();
+				byte[] combined = new byte[fileContents.length + two.length];
+				System.arraycopy(fileContents, 0, combined, 0, fileContents.length);
+				System.arraycopy(two, 0, combined, fileContents.length, two.length);
+				MqttMessage updateContent = new MqttMessage(combined);
+				thisClient.publish("EdgeDevice", updateContent);
+			} catch (Exception e) {
+				System.out.println("CID not correct");
 			}
-			else
-			{
+
+		} else if (digestMessage[0].equals("auth_request")) {
+			String decodedMAC = ccm.encode(Kiks,
+					digestMessage[0] + " " + digestMessage[1] + " " + digestMessage[2] + " " + digestMessage[3]);
+			if (digestMessage[digestMessage.length - 1].equals(decodedMAC)) {
+				IoTDevicesIDs.add(Integer.parseInt(digestMessage[1]));
+				startAuth(ccm.ccmaes(false, digestMessage[3], Km, iv));
+			} else {
 				System.out.println("Auth request2 mac check fail");
 			}
-		}
-		else if(digestMessage[0].equals("auth_request2"))
-		{
+		} else if (digestMessage[0].equals("auth_request2")) {
 			System.out.println("Auth request2 recieved");
 			// msg4 When the Gateway receives Message3
-			String message4 = IDg +" " + ccm.ccmaes(true, "Ack", Km, iv) +" ";
-			String  MAC =ccm.encode(message4 ,Kiks);
+			String message4 = IDg + " " + ccm.ccmaes(true, "Ack", Km, iv) + " ";
+			String MAC = ccm.encode(message4, Kiks);
 			MqttMessage lastMessage = new MqttMessage();
-			lastMessage.setPayload((message4+ MAC).getBytes());
-			thisClient.publish("EdgeDevice",lastMessage );
+			lastMessage.setPayload((message4 + MAC).getBytes());
+			thisClient.publish("EdgeDevice", lastMessage);
 			// msg 3 When the IoT device receives Message 2
 //			startAuth();
 		}
-		
+
 	}
 
 	private void startAuth(String nonce1) throws Exception {
@@ -124,21 +139,21 @@ public class Gateway implements MqttCallback {
 		RandFirmSeqsi = generateSetOfSeqNumbers(10);
 		RandFirmSeqsiPlus1 = generateSetOfSeqNumbers(10);
 		SecureRandom random = new SecureRandom();
-		
+
 		for (int i = 0; i < RandFirmSeqsiPlus1.length; i++) {
 			RandomSequence += RandFirmSeqsiPlus1[i] + " ";
 		}
 		nonce2 = secureRandom();
-		String encryptednonce2=  ccm.ccmaes(true, nonce2, Km, iv);
+		String encryptednonce2 = ccm.ccmaes(true, nonce2, Km, iv);
 		String MAC = ccm.encode(Kiks, nonce2 + nonce1);
 		String encryptedSequence = "";
 		String encryptedMessage = "";
 
-		encryptedMessage += ccm.ccmaes(true,nonce2, Km, iv) + ",";
+		encryptedMessage += ccm.ccmaes(true, nonce2, Km, iv) + ",";
 		for (int i = 0; i < RandFirmSeqsiPlus1.length; i++) {
 			encryptedMessage += ccm.ccmaes(true, RandFirmSeqsiPlus1[i], Km, iv) + " ";
 		}
-		encryptedMessage += "," + ccm.ccmaes(true,nonce1, Km, iv);
+		encryptedMessage += "," + ccm.ccmaes(true, nonce1, Km, iv);
 		String message2 = IDg + "," + encryptedMessage + "," + MAC;
 		MqttMessage message = new MqttMessage();
 		message.setPayload(message2.getBytes());
@@ -151,24 +166,11 @@ public class Gateway implements MqttCallback {
 		System.out.println("Message delivered! from gateway");
 
 	}
-	
+
 	static String[] generateSetOfSeqNumbers(int randoms) {
 		ccm = new CCMImpl();
-//		SecureRandom random = new SecureRandom();
-//		int[] arrayInt = new int[randoms];
-//		String[] arrayString = new String[randoms];
-//		arrayInt[0] = Math.abs(random.nextInt());
-//		arrayString[0] = Integer.toString(arrayInt[0]);
-//		for (int i = 1; i < randoms; i++) {
-//			int number;
-//			String encryptedNumber = "";
-//			do {
-//				arrayInt[i] = Math.abs(random.nextInt());
-//				encryptedNumber = ccm.ccmaes(true, Integer.toString(arrayInt[i]), Km, iv);
-//			} while (ccm.ccmaes(false, encryptedNumber, Km, iv).equals("mac check in CCM failed"));
-//			arrayString[i] = Integer.toString(arrayInt[i]);
-//		}
-		String arrayInt[] = {"234365208","247058986","859950640","886209246","1758019480","1959642116","1161690206","1406042517","734704895","640922405"};
+		String arrayInt[] = { "234365208", "247058986", "859950640", "886209246", "1758019480", "1959642116",
+				"1161690206", "1406042517", "734704895", "640922405" };
 		return arrayInt;
 	}
 
